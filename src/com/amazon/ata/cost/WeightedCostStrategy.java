f4590104e00c82691fb7d1f5b2e6697b0466712f
Aidan Au <aidan.au.linkedin@gmail.com>
2023-11-07 09:26:54 -0500

    Removed unused imports

--- src/com/amazon/ata/cost/WeightedCostStrategy.java
+++ src/com/amazon/ata/cost/WeightedCostStrategy.java
@@ -1,13 +1,9 @@
 package com.amazon.ata.cost;
 
-import com.amazon.ata.types.Material;
-import com.amazon.ata.types.Packaging;
 import com.amazon.ata.types.ShipmentCost;
 import com.amazon.ata.types.ShipmentOption;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * A strategy to calculate the cost of a ShipmentOption based on its environmental cost.
